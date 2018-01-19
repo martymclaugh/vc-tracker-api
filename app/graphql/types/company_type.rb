16083b5ce6bc38ebd8449b312d1bd4772486cc7d
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-18 16:59:51 -0800

    add venture capitalists query and slug to company type

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -2,17 +2,18 @@ Types::CompanyType = GraphQL::ObjectType.define do
   name 'Company'
 
   field :id, !types.ID
+  field :slug, !types.String
   field :name, !types.String
   field :description, !types.String
   field :budget, !types.Int
   field :raised, !types.Int
   field :timeline, !Types::DateType
-  # field :venture_capitalists do
-  #   type !types[Types::VentureCapitalistType]
-  #   argument :size, types.Int, default_value: 20
-  #   argument :number, types.Int, default_value: 1
-  #   resolve -> (company, args, ctx) {
-  #     company.venture_capitalists.limit(args[:size])
-  #   }
-  # end
+  field :ventureCapitalists do
+    type !types[Types::VentureCapitalistType]
+    argument :size, types.Int, default_value: 20
+    argument :number, types.Int, default_value: 1
+    resolve -> (company, args, ctx) {
+      company.venture_capitalists.limit(args[:size])
+    }
+  end
 end
