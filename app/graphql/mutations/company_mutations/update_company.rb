00792e18c4e205e917fdf598ea308291aa7a6960
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-18 17:13:13 -0800

    add update company mutation

---
+++ app/graphql/mutations/company_mutations/update_company.rb
@@ -0,0 +1,16 @@
+class UpdateCompany < GraphQL::Function
+  argument :id, !types.ID
+  argument :name, types.String
+  argument :description, types.String
+  argument :budget, types.Int
+  argument :raised, types.Int
+  argument :timeline, Types::DateType
+
+  type Types::CompanyType
+
+  def call(obj, args, ctx)
+    company = Company.find(args[:id])
+
+    company if company.update(args.to_h)
+  end
+end
