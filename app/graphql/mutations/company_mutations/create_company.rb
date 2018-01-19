ead61961b802a7845e4715962a8867ba1ea600e9
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-18 17:12:33 -0800

    add create company mutation

---
+++ app/graphql/mutations/company_mutations/create_company.rb
@@ -0,0 +1,15 @@
+class CreateCompany < GraphQL::Function
+  argument :name, !types.String
+  argument :description, !types.String
+  argument :budget, !types.Int
+  argument :raised, types.Int
+  argument :timeline, !Types::DateType
+
+  type Types::CompanyType
+
+  def call(obj, args, ctx)
+    slug = args[:name].gsub!(/[^0-9A-Za-z]/, ' ').downcase.split(' ').join('-');
+
+    Company.create!(args.to_h.merge(slug: slug))
+  end
+end
