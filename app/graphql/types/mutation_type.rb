ac0c7fb1cde80c18c5a48ea0f0b1079a6be3eb7e
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-18 17:12:02 -0800

    add mutation calls

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,24 @@
 Types::MutationType = GraphQL::ObjectType.define do
-  name "Mutation"
+  name 'Mutation'
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
+  # Company Mutations
+  field :createCompany, function: CreateCompany.new
+  field :updateCompany, function: UpdateCompany.new
+  field :destroyCompany, function: DestroyCompany.new
+
+  # VentureCapitalist Mutations
+  field :createVentureCapitalist, function: CreateVentureCapitalist.new
+  field :updateVentureCapitalist, function: UpdateVentureCapitalist.new
+  field :destroyVentureCapitalist, function: DestroyVentureCapitalist.new
+
+  # NotableInvestment Mutations
+  field :createNotableInvestment, function: CreateNotableInvestment.new
+  field :updateNotableInvestment, function: UpdateNotableInvestment.new
+  field :destroyNotableInvestment, function: DestroyNotableInvestment.new
+
+  # Note Mutations
+  field :createNote, function: CreateNote.new
+  field :updateNote, function: UpdateNote.new
+  field :destroyNote, function: DestroyNote.new
+
 end
