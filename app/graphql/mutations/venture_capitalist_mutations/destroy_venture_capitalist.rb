78fced1d8523cc83ccf523bbea15907d26492415
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-21 15:36:57 -0800

    destroy associations

--- app/graphql/mutations/venture_capitalist_mutations/destroy_venture_capitalist.rb
+++ app/graphql/mutations/venture_capitalist_mutations/destroy_venture_capitalist.rb
@@ -1,10 +1,11 @@
 class DestroyVentureCapitalist < GraphQL::Function
-  argument :id, !types.ID
+  argument :id, types.ID
 
   type Types::VentureCapitalistType
 
   def call(obj, args, ctx)
     venture_capitalist = VentureCapitalist.find_by_slug_or_id(args[:id])
+    venture_capitalist.companies_venture_capitalists.destroy_all
 
     venture_capitalist if venture_capitalist.destroy
   end
