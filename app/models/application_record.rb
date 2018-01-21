4a84a78417521c6b955b2e7b3dfc9e996d70eef9
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-21 15:38:40 -0800

    fix helper method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.find_by_slug_or_id(id)
-    find_by(slug: id) || VentureCapitalist.find(id)
+    find_by(slug: id) || self.find(id)
   end
 end
