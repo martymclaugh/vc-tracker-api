ba985e06ad44aee0ec94eaf7fd13081e0e84aba6
Marty McLaughlin <martymclaugh@gmail.com>
2018-01-21 15:37:41 -0800

    add args for sorting

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -8,12 +8,16 @@ Types::CompanyType = GraphQL::ObjectType.define do
   field :budget, !types.Int
   field :raised, !types.Int
   field :timeline, !Types::DateType
+  field :updated_at, !Types::DateType
   field :ventureCapitalists do
     type !types[Types::VentureCapitalistType]
     argument :size, types.Int, default_value: 20
     argument :number, types.Int, default_value: 1
+    argument :order_by, types.String, default_value: 'id'
+    argument :ascending, types.String, default_value: 'ASC'
+
     resolve -> (company, args, ctx) {
-      company.venture_capitalists.limit(args[:size])
+      company.venture_capitalists.limit(args[:size]).order("#{args[:order_by]} #{args[:ascending]}")
     }
   end
 end
